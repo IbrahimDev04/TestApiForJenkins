8867ad21d532af17bd194a786bfa4a7da7de3be4
IbrahimDev04 <tu7nluy9f@code.edu.az>
2026-07-01 01:40:28 +0400

    dockerfile edit new5

--- TestApi/Program.cs
+++ TestApi/Program.cs
@@ -1,19 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.WebHost.UseUrls("http://*:1000");
 
 var app = builder.Build();
 
-// Swagger həmişə aktiv olsun
-//app.UseSwagger();
-//app.UseSwaggerUI();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 //app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
